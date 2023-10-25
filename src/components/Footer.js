308b92d3668a03a36000aae3658187a3dd4c8729
evandalmeida <edalmei1@gmail.com>
2023-10-25 02:52:15 -0400

    everything workssss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
 export default function Footer() {
     return (
         <footer className="footer">
-                <a href="https://github.com/evandalmeida" alt="Github" target="_blank" rel="noreferrer">Github
-                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/>
-                </a>
-                <a href="https://www.linkedin.com/in/evan-d-almeida-31212922b/" alt="LinkedIn" target="_blank" rel="noreferrer">LinkedIn 
-                    <img src="https://cdn-icons-png.flaticon.com/512/61/61109.png"/>
-                </a>
-                {/* <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=claejlu@gmail.com" alt="Email" target="_blank" rel="noreferrer">
-                    Email
-                    <img src="https://cdn-icons-png.flaticon.com/512/3178/3178158.png"/>
-                </a> */}
+            <a href="https://github.com/evandalmeida" target="_blank" rel="noreferrer">
+                Github
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github" />
+            </a>
+            <a href="https://www.linkedin.com/in/evan-d-almeida-31212922b/" target="_blank" rel="noreferrer">
+                LinkedIn
+                <img src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="LinkedIn" />
+            </a>
+            {/* <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=claejlu@gmail.com" target="_blank" rel="noreferrer">
+                Email
+                <img src="https://cdn-icons-png.flaticon.com/512/3178/3178158.png" alt="Email" />
+            </a> */}
         </footer>
     )
 }
